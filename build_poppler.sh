b26b87fb031a2a3278d5fb77219e4dfdbefb4f68
yagince <natsuki.yagi@viibar.com>
2016-02-24 11:04:21 +0900

    add debug log

--- build_poppler.sh
+++ build_poppler.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+echo "[Download]"
 wget https://poppler.freedesktop.org/poppler-0.41.0.tar.xz
+
+echo "[Unarchive]"
 tar xf poppler-0.41.0.tar.xz
+
+echo "[Make & Install]"
 cd poppler
 ./configure
 make
