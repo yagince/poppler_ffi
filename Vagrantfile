c1c88e37e24cc971866020e1c4e8c9d3fc037ac1
yagince <natsuki.yagi@viibar.com>
2016-02-25 19:10:43 +0900

    fixed poppler build script

---
+++ Vagrantfile
@@ -0,0 +1,8 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+  config.vm.box = "ubuntu/vivid64"
+
+  config.vm.synced_folder ".", "/vagrant"
+end
--- build_poppler.sh
+++ build_poppler.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+#
+# build poppler-0.38.0
+#
+
+sudo apt-get install -y lbzip2 pkg-config libpng++-dev libxml2-dev libcairo2-dev
+
+wget http://downloads.sourceforge.net/freetype/freetype-2.6.3.tar.bz2
+tar xf freetype-2.6.3.tar.bz2
+$(cd 'freetype-2.6.3' && ./configure && make && sudo make install)
+
+wget https://launchpad.net/ubuntu/+archive/primary/+files/fontconfig_2.11.1.orig.tar.bz2
+tar xf fontconfig_2.11.1.orig.tar.bz2
+$(cd 'fontconfig-2.11.1' && ./configure --enable-libxml2 && make && sudo make install)
 
 wget https://launchpad.net/ubuntu/+archive/primary/+files/poppler_0.38.0.orig.tar.xz
-
-echo $(tar xvf poppler_0.38.0.orig.tar.xz)
-echo $(ls -al)
-echo $(cd poppler_0.38.0 && ./configure && make && sudo make install)
+tar xf poppler_0.38.0.orig.tar.xz
+$(cd 'poppler-0.38.0' && ./configure && make && sudo make install)
