1d36bc77f48006ea2d603b351421ebe0f7fcbcbc
yagince <natsuki.yagi@viibar.com>
2016-02-25 17:27:08 +0900

    fixed

--- build_poppler.sh
+++ build_poppler.sh
@@ -2,10 +2,6 @@
 
 wget https://launchpad.net/ubuntu/+archive/primary/+files/poppler_0.38.0.orig.tar.xz
 
-tar xvf poppler_0.38.0.orig.tar.xz
-
-ls -al
-cd poppler_0.38.0
-./configure
-make
-sudo make install
+$(tar xvf poppler_0.38.0.orig.tar.xz)
+$(ls -al)
+$(cd poppler_0.38.0 && ./configure && make && sudo make install)
