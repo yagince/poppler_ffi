5f5e534f9968959982e773c6a6797149107dc0cc
yagince <natsuki.yagi@viibar.com>
2016-02-25 17:16:55 +0900

    install tar

--- build_poppler.sh
+++ build_poppler.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-echo "[Download]"
 wget https://launchpad.net/ubuntu/+archive/primary/+files/poppler_0.38.0.orig.tar.xz
 
-echo "[Unarchive]"
-tar xf poppler_0.38.0.orig.tar.xz
+tar xvf poppler_0.38.0.orig.tar.xz
 
-echo "[Make & Install]"
 cd poppler_0.38.0
 ./configure
 make
